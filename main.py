3cdef02ceb25f3fc0317d8e679e93217d97f93a6
tonymichaelhead <tonymichaelhead@gmail.com>
2019-04-13 00:51:55 -0700

    Add basic ui with player functions

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import os
-import pygame
 from tkinter.filedialog import askdirectory
+
+import pygame
+from mutagen.id3 import ID3
 from tkinter import *
 
 root = Tk()
 root.minsize(300,300)
 
 listofsongs = []
+realnames = []
 
 index = 0
+
+def nextsong(event):
+    global index
+    index += 1
+    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.play()
+    
+def prevsong(event):
+    global index
+    index -= 1
+    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.play()
+
+def stopsong(event):
+    pygame.mixer.music.stop()
 
 def directorychooser():
 
@@ -17,8 +35,49 @@ def directorychooser():
 
     for files in os.listdir():
         if files.endswith(".mp3"):
+            realdir = os.path.realpath(files)
+            # print(audio)
+            # Run a check to see if meta data exists and if not just show filename
+            try:
+                audio = ID3(realdir)    
+                realnames.append(audio["TIT2"].text[0])
+            except:
+                realnames.append(files)
 
             listofsongs.append(files)
-            print(files)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(listofsongs[0])
+    pygame.mixer.music.play()
+
+directorychooser()
+
+label = Label(root, text='Music Player')
+label.pack()
+
+listbox = Listbox(root)
+listbox.pack()
+
+# listofsongs.reverse()
+realnames.reverse()
+
+for items in listofsongs:
+    listbox.insert(0, items)
+
+# listofsongs.reverse()
+realnames.reverse()
+
+nextbutton = Button(root, text='Next Song')
+nextbutton.pack()
+
+previousbutton = Button(root, text='Previous Song')
+previousbutton.pack()
+
+stopbutton = Button(root, text='Stop Music')
+stopbutton.pack()
+
+nextbutton.bind("<Button-1>", nextsong)    
+previousbutton.bind("<Button-1>", prevsong)    
+stopbutton.bind("<Button-1>", stopsong)    
 
 root.mainloop()
